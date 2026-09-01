87d63bbd7e05b55df032325af8d28ceeb0fcc078
Markus Erwin Nduru <email_anda@example.com>
2026-09-01 10:11:34 +0700

    update

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -7,8 +7,8 @@ import { createClient } from '@supabase/supabase-js';
 import Swal from 'sweetalert2';
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export default function LoginPage() {
